8c4721f1a4dcd9efe5ec547bed537e76e3ec281c
DeveshPatel-14 <devpatel1130@gmail.com>
2023-08-02 18:53:32 +0530

    Updated server

--- server.js
+++ server.js
@@ -5,12 +5,40 @@ import RazorPay from "razorpay";
 import nodeCron from "node-cron";
 import { Stats } from "./models/Stats.js";
 import path from 'path';
-import express from "express";
 import cors from "cors";
+import express from "express";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
 
+// Importing & Using Routes
+import course from "./routes/courseRoutes.js";
+import user from "./routes/userRoutes.js";
+import payment from "./routes/paymentRoutes.js";
+import other from "./routes/otherRoutes.js";
+
+config({
+  path: "./config/config.env",
+});
+
+const app = express();
+
+// Using Middlewares
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(cookieParser());
+
+app.use("/api/v1", course);
+app.use("/api/v1", user);
+app.use("/api/v1", payment);
+app.use("/api/v1", other);
 
 connectDB();
-
 
 const __dirname = path.resolve();
 app.use(express.static(path.resolve(__dirname, 'build')));
@@ -29,6 +57,14 @@ app.use(express.static(root))
 app.get('*', (req, res) =>
   res.sendFile(path.resolve('build', 'index.html'))
 );
+
+app.get("/", (req, res) =>
+  res.send(
+    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+  )
+);
+
+app.use(ErrorMiddleware);
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
