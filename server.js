42d86f8fbcbaa6139197d19bbaef10d1e93299c3
DeveshPatel-14 <devpatel1130@gmail.com>
2023-08-02 13:09:07 +0530

    Updated server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
